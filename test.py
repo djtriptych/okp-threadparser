9192f338ac41d50da9c3570af594aa071de8910b
Kenan Banks <kbanks@gmail.com>
2011-10-18 19:07:52 -0400

    Adding a few comments to the test script

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
+import pprint
 import threadparser
 
+# Grab the HTML of an OKP post.
 html = open('post.html').read()
+
+# And parse the post (relatively quickly!)
 TP = threadparser.ThreadParser(html)
+
+# Then you can play with the replies in the post....
+# For instance, this will show the data parsed from the 4th reply in the post.
+reply = TP.replies[3]
+pprint.pprint(reply.__dict__)
+
+# have fun...
